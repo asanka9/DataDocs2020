c100c6ed533468e05d41164643dfa92b70030428
asanka9 <sendasanka96@gmail.com>
2020-10-27 17:22:27 +0530

    sas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import {RandomForestComponent} from './data-science/machine-learning/ensemble-te
 import {KnnAlgoComponent} from './data-science/machine-learning/knn-algo/knn-algo.component';
 import {MachineLearningComponent} from './data-science/machine-learning/machine-learning.component';
 import {NlpComponent} from './data-science/nlp/nlp.component';
+import {TextPreprocessing01Component} from './data-science/nlp/text-preprocessing01/text-preprocessing01.component';
+import {TextPreprocessing02Component} from './data-science/nlp/text-preprocessing02/text-preprocessing02.component';
+import {TextPreprocessing03Component} from './data-science/nlp/text-preprocessing03/text-preprocessing03.component';
+
 
 
 
@@ -21,7 +25,11 @@ const routes: Routes = [
   {path:'Random Forest',component:RandomForestComponent},
   {path:'K-Nearest Neighbor (KNN) Algorithm',component:KnnAlgoComponent},
   {path:'What is Machine Learning?',component:MachineLearningComponent},
-  {path:'Natural Language Processing (NLP)',component:NlpComponent}
+  {path:'Natural Language Processing (NLP)',component:NlpComponent},
+  {path:'Text Prepossessing (Tokenization, Stemming, Lematization)',component:TextPreprocessing01Component},
+  {path:'Text Prepossessing (Bag Of Words)',component:TextPreprocessing02Component},
+  {path:'Text Prepossessing (Word2Vec)',component:TextPreprocessing03Component},
+
 ];
 
 @NgModule({
